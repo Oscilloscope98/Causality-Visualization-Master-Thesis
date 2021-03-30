71350f36fdbbfad9f4d6d4ee38be075e8835debf
“yuwen” <yuwen@kth.se>
2021-03-30 21:28:32 +0200

    add body templates with neck subdivision for tests

--- src/index.js
+++ src/index.js
@@ -1,17 +1,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './style.css';
+import BodyContour from './bodyComponents/bodyContour';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            symptoms: []
+        };
+    }
+
+    delectSym(symName){
+        let tempSyms = this.state.symptoms;
+        const index = tempSyms.indexOf(symName);
+            if (index > -1) {
+                tempSyms.splice(index, 1);
+            }
+        this.setState({symptoms: tempSyms});
+    }
+
+    handleClick(symName){
+        let tempSyms = this.state.symptoms;
+        if (!tempSyms.includes(symName)){
+            tempSyms.push(symName);
+            this.setState({symptoms: tempSyms});
+        }
+        else{
+            //double click to disable a symptom
+            this.delectSym(symName);
+        }
+        console.log(this.state.symptoms);
+    }
+
+
+    render() {
+        let symString = '';
+        this.state.symptoms.forEach(item => symString+=item+', ');
+        symString = symString.slice(0, -2); 
+        return (
+            <div className="center">
+                <p>Selected Symptoms: {symString}</p>
+                <BodyContour width={550} 
+                    onClick={symName => this.handleClick(symName)} 
+                    deleteSym={symName => this.delectSym(symName)}
+                    selectedSymptoms={this.state.symptoms}/>
+            </div>
+        );
+    }
+}
+
+// ========================================
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
